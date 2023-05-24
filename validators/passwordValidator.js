063db321f5186f8507d362e40d98beced3c281de
MatyášCZ <mmaly966@gmail.com>
2023-05-24 15:24:54 +0200

    commit

---
+++ validators/passwordValidator.js
@@ -0,0 +1,26 @@
+class PasswordValidator {
+    #messages = [];
+    #numbers = "1234567890";
+    #capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    #specialChars = "~!@#$%^&*_-+=`|(){}[]:;\"'<>,.?/";
+
+    validate(password) {
+        if (!(password.length >= 16 && password.length < 100)) {
+            this.#messages.push("Heslo je příliš krátké nebo dlouhé.");
+        }
+
+        if (!password.split("").some((letter) => this.#capitals.includes(letter))) {
+            this.#messages.push("Heslo neobsahuje velká písmena.");
+        }
+
+        if (!password.split("").some((letter) => this.#numbers.includes(letter))) {
+            this.#messages.push("Heslo neobsahuje čísla.");
+        }
+
+        if (!password.split("").some((letter) => this.#specialChars.includes(letter))) {
+            this.#messages.push("Heslo neobsahuje speciální znaky.");
+        }
+
+        return this.#messages;
+    }
+}
