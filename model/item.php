aabe5a193db43f62a2cd196372583c476913433b
MatyášCZ <mmaly966@gmail.com>
2023-06-05 19:21:35 +0200

    commit

---
+++ model/item.php
@@ -0,0 +1,13 @@
+<?php
+
+class Item
+{
+    public int $Id;
+    public string $Content;
+    public bool $Done;
+    public string $CreatedOn;
+    public ?string $UpdatedOn;
+    public int $GroupId;
+}
+
+?>
---
+++ model/user.php
@@ -0,0 +1,12 @@
+<?php
+
+class User
+{
+    public int $Id;
+    public string $Username;
+    public string $Password;
+    public string $FirstName;
+    public string $LastName;   
+}
+
+?>
