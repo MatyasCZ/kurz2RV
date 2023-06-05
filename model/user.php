4c84d31e1d1b561cd7c34771c9b3992ba40f7c6c
MatyášCZ <mmaly966@gmail.com>
2023-06-05 19:28:38 +0200

    commit

--- model/user.php
+++ model/user.php
@@ -6,7 +6,8 @@ class User
     public string $Username;
     public string $Password;
     public string $FirstName;
-    public string $LastName;   
+    public string $LastName; 
+    public int $OrderCount;  
 }
 
 ?>
