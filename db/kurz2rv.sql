23acf128604e8106251c9a3efa92d96d1ed977bb
MatyášCZ <mmaly966@gmail.com>
2023-06-09 17:08:12 +0200

    commit

--- db/kurz2rv.sql
+++ db/kurz2rv.sql
@@ -16,33 +16,6 @@ GDPR tinyint(1) NOT NULL,
 OrderCount int DEFAULT 0
 );
 
-DROP TABLE IF EXISTS Groups;
-CREATE TABLE Groups (
-Id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-Name varchar(500) NOT NULL,
-CreatedOn datetime NOT NULL,
-UpdatedOn datetime NULL,
-UserId int NOT NULL,
-CONSTRAINT fk_group_user
-FOREIGN KEY (UserId) REFERENCES Users(Id)
-ON DELETE CASCADE
-ON UPDATE CASCADE
-);
-
-DROP TABLE IF EXISTS Items;
-CREATE TABLE Items (
-Id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-Content varchar(5000) NOT NULL,
-Done bit NOT NULL,
-CreatedOn datetime NOT NULL,
-UpdatedOn datetime NULL,
-GroupId int NOT NULL,
-CONSTRAINT fk_item_group
-FOREIGN KEY (GroupId) REFERENCES Groups(Id)
-ON DELETE CASCADE
-ON UPDATE CASCADE
-);
-
 DROP TABLE IF EXISTS Orders;
 CREATE TABLE Orders (
 ID int PRIMARY KEY AUTO_INCREMENT,
